4ee36d090b53b40e8d5c6a132fb6292354785f9d
Rouven Himmelstein <rouven@himmelstein.info>
2023-01-21 11:33:41 +0100

    cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -15,8 +15,7 @@ void setup() {
     Serial.begin(115200);
 
     if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
-        Serial.println(F("SSD1306 allocation failed"));
-        for (;;);
+        Serial.println("SSD1306 allocation failed");
     }
 
     // Wait for display to initialize
