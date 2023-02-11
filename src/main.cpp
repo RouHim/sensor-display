0d736431ed3762ce737d85e53e10ee11d78dafa1
Rouven Himmelstein <rouven@himmelstein.info>
2023-02-11 13:13:02 +0100

    use u8g2

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,29 @@
 #include <Arduino.h>
 #include "U8g2lib.h"
 
-U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE, /* clock=*/ A5, /* data=*/ A4);
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE, A5, /* data=*/ A4);
+
+u8g2_uint_t displayWidth;
+u8g2_uint_t displayHeight;
+
+void drawString(const char *value);
 
 void setup(void) {
     u8g2.begin();
+    displayWidth = u8g2.getDisplayWidth();
+    displayHeight = u8g2.getDisplayHeight();
+    u8g2.setFont(u8g2_font_profont22_mf);
 }
 
 void loop(void) {
-    u8g2.clearBuffer();
-    u8g2.setFont(u8g2_font_ncenB14_tr);
-    u8g2.drawStr(0, 0, "Hello World!");
-    u8g2.sendBuffer();
+    drawString("25°C");
     delay(1000);
+}
+
+void drawString(const char *value) {
+    u8g2.clearBuffer();
+    u8g2_uint_t x = (displayWidth - u8g2.getUTF8Width(value)) / 2;
+    u8g2_uint_t y = (displayHeight - u8g2.getFontAscent() - u8g2.getFontDescent()) / 2 + u8g2.getFontAscent();
+    u8g2.drawUTF8(x, y, value);
+    u8g2.sendBuffer();
 }
