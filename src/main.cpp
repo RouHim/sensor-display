a294803d308017e5151d6c9493173c3aa8b10307
Rouven Himmelstein <rouven@himmelstein.info>
2023-02-15 12:42:47 +0100

    use u8g2

--- src/main.cpp
+++ src/main.cpp
@@ -8,15 +8,17 @@ void drawDataToI2cDisplay(const char *value);
 
 void parseI2cDisplayCommand(String &payload);
 
-void sendToI2cDisplay(String &command);
+void sendToI2cDisplay(const char *command);
 
 void setup(void) {
     // Initialize serial communication
     Serial.begin(115200);
+
+    // Initialize the i2c displays
+    u8g2.begin();
 }
 
 void loop(void) {
-    delay(500);
 
     // Check if there is data available on the serial port
     // If not, return
@@ -26,105 +28,96 @@ void loop(void) {
 
     String payload = Serial.readStringUntil(';');
 
-    // Print the serial payload that was received
-    Serial.print(F("Message received: "));
-    Serial.println(payload);
+    // Check if payload is good, if not, the first two characters are the length of the total payload with the ';'
+    byte expectedLength = strtol(payload.substring(0, 2).c_str(), nullptr, 10);
+    if (expectedLength != payload.length()) {
+        return;
+    }
+
+    // If the payload is good, remove the first two characters
+    payload.remove(0, 2);
 
     // Read the first character of the payload
     char messageType = payload.charAt(0);
-    Serial.print("messageType: ");
-    Serial.println(messageType);
+
+    // And remove this from the payload
+    payload.remove(0, 1);
 
     // Check if the payload starts with a 'i' to identify an i2c command
     if (messageType == 'i') {
-        Serial.println("i2c command received");
-
-        // Remove the first character from the payload
-        payload.remove(0, 1);
 
         // Parse the payload as an i2c command
         parseI2cDisplayCommand(payload);
     }
+
+    // Wait a bit
+    delay(10);
 }
 
 /// Parses the payload as an i2c command and sends the data to the displays
-/// Example: 0x3C070°C,0x5C051%
+/// Example: 0x3C2137°C,0x5C2137%,...
 /// Syntax: <i2c-address><font-size><data>,<i2c-address><font-size><data>,...
 void parseI2cDisplayCommand(String &payload) {
     // Each display data command is separated by a comma
     // Split by comma and iterate over to send the data to the displays
     int commaIndex = payload.indexOf(',');
-    Serial.print("commaIndex: ");
-    Serial.println(commaIndex);
 
-    // if there is no comma, send the payload to the display
+    // if there is no comma, send the payload to the only display
     if (commaIndex == -1) {
-        sendToI2cDisplay(payload);
+        sendToI2cDisplay(payload.c_str());
         return;
     }
 
+    // If there is any (multiple possible), send the data to the corresponding display
     while (commaIndex != -1) {
-        // Get the display data command
-        String displayDataCommand = payload.substring(0, commaIndex);
-        Serial.print("displayDataCommand: ");
-        Serial.println(displayDataCommand);
-
-        // Send the display data command to the display
-        sendToI2cDisplay(displayDataCommand);
-
-        // Remove the display data command from the payload
+        // Get the command
+        String command = payload.substring(0, commaIndex);
+        // Remove the command from the payload
         payload.remove(0, commaIndex + 1);
+        // Send the command to the display
+        sendToI2cDisplay(command.c_str());
 
-        // Find the next comma
+        // Get the next comma index
         commaIndex = payload.indexOf(',');
-        Serial.print("commaIndex: ");
-        Serial.println(commaIndex);
     }
+
+    // Send the command to the last display
+    sendToI2cDisplay(payload.c_str());
 }
 
 /// Sends the payload to a display
 /// Example payload: 0x3C070°C
-void sendToI2cDisplay(String &command) {
-    // Print the command that was received
-
-    int length = strlen(command.c_str());
-    Serial.print(F("Command received: "));
-    Serial.println(command.c_str()[length - 1]);
-
+void sendToI2cDisplay(const char *command) {
     /// Determine the i2c address
-    // Get the 4 first characters of the payload, without using the substring method
-    // Omit the first two characters, because they are just indicating a hex string
-    char i2cAddressHexString[3];
-    i2cAddressHexString[0] = command.charAt(2);
-    i2cAddressHexString[1] = command.charAt(3);
-    i2cAddressHexString[2] = '\0';
-    // Then convert the hex string to a small integer
-    const byte i2cAddress = strtol(i2cAddressHexString, nullptr, 16);
-    // Set the i2c address
-    u8g2.setI2CAddress(i2cAddress * 2);
-
-    /// Set the font size
-    switch (command.charAt(4)) {
-        default:
-        case 0:
-            u8g2.setFont(u8g2_font_profont10_mf);
-            break;
-        case 1:
-            u8g2.setFont(u8g2_font_profont17_mf);
-            break;
-        case 2:
-            u8g2.setFont(u8g2_font_profont29_mf);
-            break;
+    // First get the hex string, which are the 3rd and 4th characters
+    // Ensure the first two characters are 0x if not return with error message
+    if (command[0] != '0' || command[1] != 'x') {
+        return;
     }
 
-    /// Get the actual data to display
-    /// The data starts at the 5th character, without using the substring method
-    String data = command.substring(5);
-    Serial.print("data: ");
-    Serial.println(data);
+    // Convert only the first 4 characters to a hex string and set the i2c address
+    char addressAsHex[4];
+    strncpy(addressAsHex, command, 4);
 
-    // Draw the data to the display
-    drawDataToI2cDisplay(data.c_str());
+    // Set the i2c address
+    u8g2.setI2CAddress(strtol(addressAsHex, nullptr, 16) * 2);
+
+    /// Set the font size that is located in the 5th character
+    // WARNING: using three different fonts takes a lot of flash memory
+    if (command[4] == '0') {
+        u8g2.setFont(u8g2_font_profont10_mf);
+    } else if (command[4] == '1') {
+        u8g2.setFont(u8g2_font_profont17_mf);
+    } else {
+        u8g2.setFont(u8g2_font_profont29_mf);
+    }
+
+    // Copy the rest of the command to pass it to the draw function
+    char dataToShow[strlen(command) - 5];
+    strcpy(dataToShow, command + 5);
+
+    // Finally, send the data to the display
+    drawDataToI2cDisplay(dataToShow);
 }
 
 void drawDataToI2cDisplay(const char *value) {
