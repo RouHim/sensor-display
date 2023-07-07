9d565982662e3ab0ce795d7988b94a15a4ce1a04
Rouven Himmelstein <rouven@himmelstein.info>
2023-07-07 08:53:41 +0200

    feat(display): receive via tcp and deserialize

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,27 @@
-use std::io::Read;
-use std::time::Duration;
-use serialport::*;
+use message_io::network::{NetEvent, Transport};
+use message_io::node::{self};
+use sensor_core::TransferData;
 
 fn main() {
-    let port_name = "/dev/ttyUSB0";
-    let mut port = serialport::new(port_name, 12000)
-        .timeout(Duration::from_millis(100))
-        .open()
-        .expect("Failed to open port");
+    let (handler, listener) = node::split::<()>();
 
-    let mut serial_buf: Vec<u8> = vec![0; 100];
-    loop {
-        match port.read(serial_buf.as_mut_slice()) {
-            Ok(t) => {
-                println!("Read {} bytes from serial port", t);
-                println!("{:?}", serial_buf);
-            }
-            Err(ref e) if e.kind() == std::io::ErrorKind::TimedOut => (),
-            Err(e) => eprintln!("{:?}", e),
+    // Listen for TCP, UDP and WebSocket messages at the same time.
+    handler
+        .network()
+        .listen(Transport::FramedTcp, "0.0.0.0:10489")
+        .unwrap();
+
+    // Read incoming network events.
+    listener.for_each(move |event| match event.network() {
+        NetEvent::Connected(_, _) => unreachable!(), // Used for explicit connections.
+        NetEvent::Accepted(_endpoint, _listener) => println!("Client connected"),
+        NetEvent::Message(_, data) => {
+            let data: TransferData = deserialize(data);
         }
-    }
+        NetEvent::Disconnected(_endpoint) => println!("Client disconnected"),
+    });
+}
 
+fn deserialize(data: &[u8]) -> TransferData {
+    serde::Deserialize::deserialize(&mut rmp_serde::Deserializer::new(data)).unwrap()
 }
