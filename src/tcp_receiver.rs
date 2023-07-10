f1dd982c06defec9f73c33ebc01b7bdf84ee49b1
Rouven Himmelstein <rouven@himmelstein.info>
2023-07-10 09:28:10 +0200

    feat(app): refactor

--- src/tcp_receiver.rs
+++ src/tcp_receiver.rs
@@ -29,58 +29,60 @@ pub fn receive(
         match event.network() {
             NetEvent::Connected(_, _) => unreachable!(), // Used for explicit connections.
             NetEvent::Accepted(_endpoint, _listener) => println!("Client connected"),
-            NetEvent::Message(_, data) => {
-                println!("Received data with length {}", data.len());
-
-                // Measure deserialization and rendering time
-                let start = std::time::Instant::now();
-
-                let data: TransferData = deserialize(data);
-                let image_data = sensor_core::render_lcd_image(data.lcd_config, data.sensor_values);
-
-                let lcd_render_time = std::time::Instant::now();
-                println!(
-                    "Deserialization and rendering took {} ms",
-                    lcd_render_time.duration_since(start).as_millis()
-                );
-
-                // Create a Vec<u8> buffer to write the image to it
-                let mut buf = Vec::new();
-                let mut cursor = Cursor::new(&mut buf);
-                image_data
-                    .write_to(&mut cursor, ImageOutputFormat::Png)
-                    .unwrap();
-
-                // Reset the cursor to the beginning of the buffer
-                cursor.seek(SeekFrom::Start(0)).unwrap();
-
-                // Log time it took to write image to buffer
-                let write_image_to_buffer_time = std::time::Instant::now();
-                println!(
-                    "Writing image to buffer took {} ms",
-                    write_image_to_buffer_time
-                        .duration_since(lcd_render_time)
-                        .as_millis()
-                );
-
-                let image = RetainedImage::from_image_bytes("test", buf.as_slice()).unwrap();
-
-                // Log time it took to create RetainedImage
-                let create_retained_image_time = std::time::Instant::now();
-                println!(
-                    "Creating RetainedImage took {} ms",
-                    create_retained_image_time
-                        .duration_since(write_image_to_buffer_time)
-                        .as_millis()
-                );
-
-                // Write image data to mutex
-                let mut mutex = write_image_data_mutex.lock().unwrap();
-                *mutex = Some(image);
-            }
+            NetEvent::Message(_, data) => render_image(&write_image_data_mutex, data),
             NetEvent::Disconnected(_endpoint) => println!("Client disconnected"),
         }
     });
+}
+
+fn render_image(write_image_data_mutex: &Arc<Mutex<Option<RetainedImage>>>, data: &[u8]) {
+    println!("Received data with length {}", data.len());
+
+    // Measure deserialization and rendering time
+    let start = std::time::Instant::now();
+
+    let data: TransferData = deserialize(data);
+    let image_data = sensor_core::render_lcd_image(data.lcd_config, data.sensor_values);
+
+    let lcd_render_time = std::time::Instant::now();
+    println!(
+        "Deserialization and rendering took {} ms",
+        lcd_render_time.duration_since(start).as_millis()
+    );
+
+    // Create a Vec<u8> buffer to write the image to it
+    let mut buf = Vec::new();
+    let mut cursor = Cursor::new(&mut buf);
+    image_data
+        .write_to(&mut cursor, ImageOutputFormat::Png)
+        .unwrap();
+
+    // Reset the cursor to the beginning of the buffer
+    cursor.seek(SeekFrom::Start(0)).unwrap();
+
+    // Log time it took to write image to buffer
+    let write_image_to_buffer_time = std::time::Instant::now();
+    println!(
+        "Writing image to buffer took {} ms",
+        write_image_to_buffer_time
+            .duration_since(lcd_render_time)
+            .as_millis()
+    );
+
+    let image = RetainedImage::from_image_bytes("test", buf.as_slice()).unwrap();
+
+    // Log time it took to create RetainedImage
+    let create_retained_image_time = std::time::Instant::now();
+    println!(
+        "Creating RetainedImage took {} ms",
+        create_retained_image_time
+            .duration_since(write_image_to_buffer_time)
+            .as_millis()
+    );
+
+    // Write image data to mutex
+    let mut mutex = write_image_data_mutex.lock().unwrap();
+    *mutex = Some(image);
 }
 
 fn deserialize(data: &[u8]) -> TransferData {
