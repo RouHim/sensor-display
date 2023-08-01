98bbdc1fe324e3e68f3523fcb08e3d295951df6a
Rouven Himmelstein <rouven@himmelstein.info>
2023-08-01 13:57:38 +0200

    feat(renderer): improve performance

--- src/renderer.rs
+++ src/renderer.rs
@@ -1,16 +1,14 @@
+use std::sync::{Arc, Mutex};
+
+use eframe::egui::ColorImage;
 use egui_extras::RetainedImage;
-use image::ImageOutputFormat;
 use log::info;
 use sensor_core::RenderData;
-use std::io::Cursor;
-use std::sync::{Arc, Mutex};
-use eframe::egui::ColorImage;
 
 pub fn render_image(
     ui_display_image_handle: &Arc<Mutex<Option<RetainedImage>>>,
     render_data: RenderData,
 ) {
-    // Measure deserialization and rendering time
     let start = std::time::Instant::now();
 
     let image_data =
@@ -22,30 +20,20 @@ pub fn render_image(
         lcd_render_time.duration_since(start).as_millis()
     );
 
-    // Create a Vec<u8> buffer to write the image to it
-    let mut buf = Vec::new();
-    let mut cursor = Cursor::new(&mut buf);
-    image_data
-        .write_to(&mut cursor, ImageOutputFormat::Bmp)
-        .unwrap();
-
-    // Log time it took to write image to buffer
-    let write_image_to_buffer_time = std::time::Instant::now();
-    info!(
-        "Writing image to buffer took {} ms",
-        write_image_to_buffer_time
-            .duration_since(lcd_render_time)
-            .as_millis()
+    let image = RetainedImage::from_color_image(
+        "rendered_image",
+        ColorImage::from_rgba_unmultiplied(
+            [image_data.width() as usize, image_data.height() as usize],
+            image_data.as_raw(),
+        ),
     );
-
-    let image = RetainedImage::from_image_bytes("test", buf.as_slice()).unwrap();
 
     // Log time it took to create RetainedImage
     let create_retained_image_time = std::time::Instant::now();
     info!(
         "Creating RetainedImage took {} ms",
         create_retained_image_time
-            .duration_since(write_image_to_buffer_time)
+            .duration_since(lcd_render_time)
             .as_millis()
     );
 
@@ -53,6 +41,9 @@ pub fn render_image(
     let mut mutex = ui_display_image_handle.lock().unwrap();
     *mutex = Some(image);
 
-    info!("Total time: {} ms", create_retained_image_time.duration_since(start).as_millis());
+    info!(
+        "Total time: {} ms",
+        create_retained_image_time.duration_since(start).as_millis()
+    );
     info!("---");
 }
