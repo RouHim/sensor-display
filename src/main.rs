8e61873368f498f8178304123c146054b7cb8eb3
Rouven Himmelstein <rouven@himmelstein.info>
2023-11-13 09:51:23 +0100

    fix: memory leak

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use std::{env, fs, thread};
 use std::ops::Deref;
 #[cfg(unix)]
 use std::os::unix::process::CommandExt;
 use std::process::Command;
 use std::sync::{Arc, Mutex};
 use std::time::Duration;
+use std::{env, fs, thread};
 
 use eframe::egui;
 use eframe::egui::{ImageSource, Vec2};
@@ -15,6 +15,10 @@ use crate::tcp_receiver::get_local_ip_address;
 
 mod renderer;
 mod tcp_receiver;
+
+type ImageData = Vec<u8>;
+type ImageHandle = Option<(u128, ImageData)>;
+type SharedImageHandle = Arc<Mutex<ImageHandle>>;
 
 fn main() -> Result<(), eframe::Error> {
     // Set the app name for the dynamic cache folder detection
@@ -40,8 +44,8 @@ fn main() -> Result<(), eframe::Error> {
         ..Default::default()
     };
 
-    // Create arc mutex for image data
-    let image_data_mutex: Arc<Mutex<Option<Vec<u8>>>> = Arc::new(Mutex::new(None));
+    // Create handler for asynchronous image data rendering
+    let image_data_mutex: SharedImageHandle = Arc::new(Mutex::new(None));
 
     // Create new thread to listen for tcp messages
     let write_image_data_mutex = image_data_mutex.clone();
@@ -53,9 +57,10 @@ fn main() -> Result<(), eframe::Error> {
     let ip = get_local_ip_address().join(", ").clone();
     let hostname = hostname::get().unwrap().into_string().unwrap();
 
-    let show_start_screen = Arc::new(Mutex::new(true));
-    let current_frame_number: Arc<Mutex<Option<u64>>> = Arc::new(Mutex::new(None));
+    // Holds the ids (timestamps) of the cached images
+    let cached_image_index: Arc<Mutex<Vec<u128>>> = Arc::new(Mutex::new(Vec::new()));
 
+    // Render loop
     eframe::run_simple_native("Sensor Display", options, move |ctx, _frame| {
         let resolution = format!(
             "{}x{}",
@@ -63,48 +68,60 @@ fn main() -> Result<(), eframe::Error> {
             ctx.screen_rect().height()
         );
 
+        // Install image loaders
         egui_extras::install_image_loaders(ctx);
-        ctx.request_repaint_after(Duration::from_millis(250));
+
+        // Do not show the cursor
         ctx.set_cursor_icon(egui::CursorIcon::None);
+
+        // Reduced display frequency to reduce system load
+        ctx.request_repaint_after(Duration::from_millis(250));
 
         egui::Area::new("main_area")
             .fixed_pos(egui::pos2(0.0, 0.0))
             .show(ctx, |ui| {
                 let mut image_mutex = image_data_mutex.lock().unwrap();
-                if let Some(image_data) = image_mutex.deref() {
-                    // get current frame number
-                    let mut current_number = current_frame_number.lock().unwrap();
-                    let old_showing_frame = current_number.unwrap_or_default();
+                let mut cached_image_index = cached_image_index.lock().unwrap();
 
-                    let frame_number = ctx.frame_nr();
-                    let image_source =
-                        ImageSource::from((format!("bytes://{frame_number}.jpg"), image_data.clone()));
+                // A new image was rendered
+                if let Some(image_data) = image_mutex.deref() {
+                    // get timestamp of the new rendered image
+                    let render_timestamp = image_data.0;
+
+                    // Show new rendered image on the screen (also caches the image by name in memory)
+                    let image_source = ImageSource::from((
+                        format!("bytes://{render_timestamp}.jpg"),
+                        image_data.1.clone(),
+                    ));
                     let image = egui::Image::new(image_source).fit_to_exact_size(Vec2::new(
                         ctx.screen_rect().width(),
                         ctx.screen_rect().height(),
                     ));
                     ui.add(image);
 
-                    // set image_mutex to none
+                    // Set image mutex to none / consumed
                     *image_mutex = None;
 
-                    // Set current frame number
-                    *current_number = Some(frame_number);
+                    // Insert the current showing image data id to the beginning of cached image ids
+                    cached_image_index.insert(0, render_timestamp);
 
-                    // Remove the image cache for old curren showing frame number
-                    ctx.forget_image(format!("bytes://{old_showing_frame}.jpg").as_str());
+                    // Remove all images, expect the first one from the cache
+                    cached_image_index.iter().skip(1).for_each(|cache_entry| {
+                        ctx.forget_image(format!("bytes://{cache_entry}.jpg").as_str());
+                    });
 
-                    // set show_start_screen to false
-                    let mut mutex = show_start_screen.lock().unwrap();
-                    *mutex = false;
-
-                } else if *show_start_screen.lock().unwrap() {
+                    // Remove all ids expect the first one from the cache index
+                    cached_image_index.truncate(1);
+                }
+                // No new freshly rendered image or cached image available, show standby text
+                else if cached_image_index.is_empty() {
                     ui.label(&build_standby_text(&ip, &hostname, &resolution));
                 }
-                // Just show the cached image
+                // Show the cached image
                 else {
-                    let frame_number = current_frame_number.lock().unwrap().unwrap();
-                    let image_source = ImageSource::from((format!("bytes://{frame_number}.jpg"), Vec::new()));
+                    let frame_number = cached_image_index.first().unwrap();
+                    let image_source =
+                        ImageSource::from((format!("bytes://{frame_number}.jpg"), Vec::new()));
                     let image = egui::Image::new(image_source).fit_to_exact_size(Vec2::new(
                         ctx.screen_rect().width(),
                         ctx.screen_rect().height(),
@@ -115,6 +132,7 @@ fn main() -> Result<(), eframe::Error> {
     })
 }
 
+/// Builds the standby text
 fn build_standby_text(local_ip: &str, hostname: &str, display_resolution: &str) -> String {
     format!(
         "No data received yet.\n\nVersion:\t\t\t\t\t\t{}\nIP Addresse:\t\t\t\t{}\nHostname:\t\t\t\t\t{}\nDisplay resolution:\t{}",
@@ -125,7 +143,7 @@ fn build_standby_text(local_ip: &str, hostname: &str, display_resolution: &str) 
     )
 }
 
-/// Check for updates
+/// Checks for updates
 /// If an update is available, download and install it
 /// If no update is available, do nothing
 /// Automatically restart the application after update
